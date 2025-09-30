0cca40b22ed1670b5bf893b242439e4971b05c4c
Yurij Ol <2010nwa@gmail.com>
2025-09-30 23:30:57 +0300

    дфÐaddd resourse task and example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
+import { ExampleModule } from './example/example.module';
 
 @Module({
-  imports: [],
+  // для доступа к переменным окружения - ConfigModule.forRoot()
+  imports: [ConfigModule.forRoot(), TaskModule, ExampleModule],
   controllers: [AppController],
   providers: [AppService],
 })
