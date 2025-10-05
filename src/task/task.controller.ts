87077e9c248d9c47cc99473e8cac2a37cba8790a
Yurij Oleynik <2010nwa@gmail.com>
2025-10-05 13:31:36 +0300

    temp

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+
+  @Get('all')
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.taskService.findById(id);
+  }
+
+  @Post()
+  create() {
+    return this.taskService.create();
+  }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  private tasks = [
+    {
+      id: 1,
+      title: 'Task 1',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: 'Task 2',
+      isCompleted: true,
+    },
+  ];
+  findAll() {
+    return this.tasks;
+  }
+  findById(id: string) {
+    const task = this.tasks.find((task) => task.id === +id);
+    if (!task) {
+      // генерация встроенной ошибки
+      throw new NotFoundException({
+        message: 'Task not found!!!',
+        code: 'TASK_NOT_FOUND',
+        status: 404,
+      });
+    }
+    return task;
+  }
+  create() {
+    const newTask = {
+      id: this.tasks.length + 1,
+      title: 'New Task',
+      isCompleted: false,
+    };
+    this.tasks.push(newTask);
+    return this.tasks;
+  }
+}
